5129882273797837860f6d014991815ecbf2039e
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-25 20:52:49 +0900

    ✨ UPDATE: apply filters to todos api
    GET /api/todos?filters=userId:int,completed:true/false

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -4,10 +4,48 @@ function paginate(array, pageSize, pageNumber) {
   return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
 }
 
+const parseFilters = (filters) => {
+  if (!filters || typeof filters !== "string") {
+    return {};
+  }
+
+  return filters.split(",").reduce((result, filter) => {
+    try {
+      const [key, ...values] = filter.split(":");
+      if (key && values) {
+        result[key] = values.join(", ");
+      }
+    } catch (err) {}
+    return result;
+  }, {});
+};
+
+const getFilteredTodos = (todos, filters) => {
+  if (!filters || Object.keys(filters).length === 0) {
+    return todos;
+  }
+
+  return todos.filter((todo) => {
+    let matched = true;
+    if (filters?.userId) {
+      matched = matched && todo.userId === parseInt(filters.userId);
+    }
+    if (filters?.completed) {
+      const completed = JSON.parse(filters.completed.toLowerCase());
+      matched = matched && todo.completed === completed;
+    }
+
+    return matched;
+  });
+};
+
 export default (req, res) => {
   const page = parseInt(req.query?.page) || 1;
   const per_page = parseInt(req.query?.per_page) || 10;
-  const total = todos.length;
+  const filters = parseFilters(req.query?.filters);
+  const filteredTodos = getFilteredTodos(todos, filters);
+
+  const total = filteredTodos.length;
   const total_page = Math.ceil(total / per_page);
   const has_next = page < total_page;
   const pagination = {
@@ -18,7 +56,7 @@ export default (req, res) => {
     has_next,
   };
   res.status(200).json({
-    data: paginate(todos, per_page, page),
+    data: paginate(filteredTodos, per_page, page),
     pagination,
   });
 };
