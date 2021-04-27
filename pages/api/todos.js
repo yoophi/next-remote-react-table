d28ef25c1bddb3b9e99e936050b42f332abe535c
Pyunghyuk Yoo <yoophi@gmail.com>
2021-04-27 12:31:27 +0900

    ✨ IMPROVE: apply sort

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -20,6 +20,23 @@ const parseFilters = (filters) => {
   }, {});
 };
 
+const parseSort = (sort) => {
+  if (!sort || typeof sort !== "string") {
+    return {};
+  }
+
+  const result = {};
+  try {
+    const [key, ...values] = sort.split(":");
+    if (key && values) {
+      result.key = key;
+      result.ascending = values.join("").toLowerCase() === "asc";
+    }
+  } catch (err) {}
+
+  return result;
+};
+
 const getFilteredTodos = (todos, filters) => {
   if (!filters || Object.keys(filters).length === 0) {
     return todos;
@@ -39,13 +56,40 @@ const getFilteredTodos = (todos, filters) => {
   });
 };
 
+const getSortedTodos = (todos, sort) => {
+  if (!sort || Object.keys(sort).length === 0) {
+    return todos;
+  }
+
+  return todos.sort((a, b) => {
+    const direction = sort.ascending ? 1 : -1;
+    if (sort.key === "userId") {
+      if (a.userId === b.userId) {
+        return 0;
+      }
+
+      return direction * (a.userId > b.userId ? 1 : -1);
+    } else if (sort.key === "title") {
+      if (a.title === b.title) {
+        return 0;
+      }
+
+      return direction * (a.title > b.title ? 1 : -1);
+    }
+
+    return 0;
+  });
+};
+
 export default (req, res) => {
   const page = parseInt(req.query?.page) || 1;
   const per_page = parseInt(req.query?.per_page) || 10;
   const filters = parseFilters(req.query?.filters);
+  const sort = parseSort(req.query?.sort);
   const filteredTodos = getFilteredTodos(todos, filters);
+  const sortedTodos = getSortedTodos(filteredTodos, sort);
 
-  const total = filteredTodos.length;
+  const total = sortedTodos.length;
   const total_page = Math.ceil(total / per_page);
   const has_next = page < total_page;
   const pagination = {
@@ -56,7 +100,7 @@ export default (req, res) => {
     has_next,
   };
   res.status(200).json({
-    data: paginate(filteredTodos, per_page, page),
+    data: paginate(sortedTodos, per_page, page),
     pagination,
   });
 };
